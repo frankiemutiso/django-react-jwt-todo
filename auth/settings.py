23a370d44db7aaedeffa1fe4eedb991f3e781ab9
Francis Mutiso <franckmutiso@gmail.com>
2021-01-13 19:11:48 +0300

    ran build to include new changes in react

--- auth/settings.py
+++ auth/settings.py
@@ -1,13 +1,18 @@
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mirp0r5ynpj9qlpk#3d9)^&)5^od7kx=%pfzv^qls(-4vr5#8j'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
